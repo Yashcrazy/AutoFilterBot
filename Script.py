9ee317ca25c0802fcdb109bf6727a38851a10991
Yashcrazy <132813838+Yashcrazy@users.noreply.github.com>
2023-07-28 20:24:49 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -7,7 +7,7 @@ class script(object):
 
 <b>✦ ɪᴛs ᴇᴀsʏ ᴛᴏ ᴜsᴇ ᴍᴇ. ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴍᴀᴋᴇ ᴍᴇ ᴀᴅᴍɪɴ</b>"""
 
-    MY_ABOUT_TXT = """<b>✯ ᴍᴀɪɴ ʙᴏᴛ:  <a href=https://t.me/BotTest_MsBot>ᴀᴜᴛᴏ ꜰɪʟᴛᴇʀ ʙᴏᴛ</a></b>
+    MY_ABOUT_TXT = """<b>✯ ᴍᴀɪɴ ʙᴏᴛ:  <a href=https://t.me/Pirates_Titansbot>ᴀᴜᴛo ꜰɪʟᴛᴇʀ ʙᴏᴛ</a></b>
 <b>✯ Cʀᴇᴀᴛᴏʀ:  <a href=http://t.me/yashu1515>ROYAL BOTS</a></b>
 <b>✯ ᴍᴏᴠɪᴇꜱ: <a href=https://t.me/RCrequestgroup>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>
 <b>✯ ᴜᴩᴅᴀᴛᴇꜱ:  <a href=https://t.me/RoyalbotsSupport>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>"""
